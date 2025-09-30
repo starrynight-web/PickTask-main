2a7a775e3f0f172fea68839909e200ec068fa214
starrynight-web <skywatcher181@gmail.com>
2025-09-30 16:55:58 +0600

    added

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,43 @@
 from django.urls import path
 from . import views
+from django.views.generic import RedirectView  
+
 
 urlpatterns = [
     path('', views.home, name="home"),
     path('workspaces/', views.workspaces, name="workspaces"),
+    path('feature/', views.feature, name="feature"),
     path('tasks/', views.tasks, name="tasks"),
+    path('feature.html', RedirectView.as_view(pattern_name='feature')),
+     # About Us
+    path('about-us/', views.about_us, name='about_us'),
+    path('about_us.html', RedirectView.as_view(pattern_name='about_us')),
+
+    # Blog
+    path('blog/', views.blog, name='blog'),
+    path('blog.html', RedirectView.as_view(pattern_name='blog')),
+
+    # Contact
+    path('contact/', views.contact, name='contact'),
+    path('contact.html', RedirectView.as_view(pattern_name='contact')),
+
+    # Documentation
+    path('documentation/', views.documentation, name='documentation'),
+    path('documentation.html', RedirectView.as_view(pattern_name='documentation')),
+
+    # Help & Support
+    path('help-support/', views.help_support, name='help_support'),
+    path('help_support.html', RedirectView.as_view(pattern_name='help_support')),
+
+    # Integrations
+    path('integrations/', views.integrations, name='integrations'),
+    path('integrations.html', RedirectView.as_view(pattern_name='integrations')),
+
+    # Pricing
+    path('pricing/', views.pricing, name='pricing'),
+    path('pricing.html', RedirectView.as_view(pattern_name='pricing')),
+
+    # Tutorial
+    path('tutorial/', views.tutorial, name='tutorial'),
+    path('tutorial.html', RedirectView.as_view(pattern_name='tutorial')),
 ]
--- home/views.py
+++ home/views.py
@@ -5,7 +5,31 @@ from .models import Workspace, Task
 
 def home(request):
     return render(request, "home/index.html")
+def feature(request):
+    return render(request, "home/feature.html")
+def about_us(request):
+    return render(request, 'home/about_us.html')
 
+def blog(request):
+    return render(request, 'home/blog.html')
+
+def contact(request):
+    return render(request, 'home/contact.html')
+
+def documentation(request):
+    return render(request, 'home/documentation.html')
+
+def help_support(request):
+    return render(request, 'home/help_support.html')
+
+def integrations(request):
+    return render(request, 'home/integrations.html')
+
+def pricing(request):
+    return render(request, 'home/pricing.html')
+
+def tutorial(request):
+    return render(request, 'home/tutorial.html')
 def workspaces(request):
     data = Workspace.objects.all()
     return render(request, "home/workspaces.html", {"workspaces": data})
