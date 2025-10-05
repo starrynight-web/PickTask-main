4c014cbbf588f0ce2dd1871cd8de62a46e687481
Mahtabul Al Nahian <mahtabulalnahian@gmail.com>
2025-10-05 14:17:42 +0600

    fixed some more links of the static pages

--- core/urls.py
+++ core/urls.py
@@ -38,9 +38,9 @@ urlpatterns = [
     path('', include('home.urls')),
     path('auth/', include('authentication.urls')),
     path('workspace/', include('workspace.urls', namespace='workspace')),
-     path('kanban/', include('kanban.urls')), 
-     path('task/', include('task.urls')),
-      path('team/', include('team.urls')),
-      path('activity/', include('activity.urls')),
-   
+    path('kanban/', include('kanban.urls')),
+    path('task/', include('task.urls')),
+    path('team/', include('team.urls')),
+    path('activity/', include('activity.urls')),
+
 ]
--- home/urls.py
+++ home/urls.py
@@ -1,34 +1,37 @@
 from django.urls import path
 from . import views
-from django.views.generic import RedirectView  
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
     path('', views.home, name="home"),
-    #path('workspaces/', views.workspaces, name="workspaces"),
+    path('index/', views.home, name='index'),
+    path('index.html', RedirectView.as_view(pattern_name='index')),
+    # path('workspaces/', views.workspaces, name="workspaces"),
     path('feature/', views.feature, name="feature"),
-    #path('tasks/', views.tasks, name="tasks"),
+    # path('tasks/', views.tasks, name="tasks"),
     path('feature.html', RedirectView.as_view(pattern_name='feature')),
-    
+
     path('about_us/', views.about_us, name='about_us'),
     path('about_us.html', RedirectView.as_view(pattern_name='about_us')),
 
     path('blog/', views.blog, name='blog'),
     path('blog.html', RedirectView.as_view(pattern_name='blog')),
 
- 
+
     path('contact/', views.contact, name='contact'),
     path('contact.html', RedirectView.as_view(pattern_name='contact')),
 
- 
-    path('documentation/', views.documentation, name='documentation'),
-    path('documentation.html', RedirectView.as_view(pattern_name='documentation')),
 
-  
+    path('documentation/', views.documentation, name='documentation'),
+    path('documentation.html', RedirectView.as_view(
+        pattern_name='documentation')),
+
+
     path('help_support/', views.help_support, name='help_support'),
     path('help_support.html', RedirectView.as_view(pattern_name='help_support')),
 
-   
+
     path('integrations/', views.integrations, name='integrations'),
     path('integrations.html', RedirectView.as_view(pattern_name='integrations')),
 
@@ -36,7 +39,7 @@ urlpatterns = [
     path('pricing/', views.pricing, name='pricing'),
     path('pricing.html', RedirectView.as_view(pattern_name='pricing')),
 
-   
+
     path('tutorial/', views.tutorial, name='tutorial'),
     path('tutorial.html', RedirectView.as_view(pattern_name='tutorial')),
 ]
